317152a14f33195dd531cc72eafd896d55d21600
Harman Khangura <hakh@collectionhouse.com.au>
2025-01-23 12:10:57 +1000

    Refactored TennisGame1

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -1,80 +1,53 @@
 ﻿namespace Tennis;
 
-public class TennisGame1 : ITennisGame
+public class TennisGame1(string player1Name, string player2Name) : ITennisGame
 {
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private string player1Name;
-    private string player2Name;
-
-    public TennisGame1(string player1Name, string player2Name)
-    {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
+    private int _player1Score = 0;
+    private int _player2Score = 0;
+    private readonly string _player1Name = player1Name;
+    private readonly string _player2Name = player2Name;
 
     public void WonPoint(string playerName)
     {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName == _player1Name)
+            _player1Score += 1;
         else
-            m_score2 += 1;
+            _player2Score += 1;
     }
 
     public string GetScore()
     {
-        string score = "";
-        var tempScore = 0;
-        if (m_score1 == m_score2)
+        if (_player1Score == _player2Score)
         {
-            switch (m_score1)
+            return _player1Score switch
             {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+                0 => "Love-All",
+                1 => "Fifteen-All",
+                2 => "Thirty-All",
+                _ => "Deuce",
+            };
+        }
 
-            }
-        }
-        else if (m_score1 >= 4 || m_score2 >= 4)
+        if (_player1Score >= 4 || _player2Score >= 4)
         {
-            var minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
-        }
-        else
-        {
-            for (var i = 1; i < 3; i++)
+            var scoreDifference = _player1Score - _player2Score;
+            return scoreDifference switch
             {
-                if (i == 1) tempScore = m_score1;
-                else { score += "-"; tempScore = m_score2; }
-                switch (tempScore)
-                {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
-            }
+                1 => "Advantage player1",
+                -1 => "Advantage player2",
+                >= 2 => "Win for player1",
+                _ => "Win for player2",
+            };
         }
-        return score;
+
+        return $"{GetScoreName(_player1Score)}-{GetScoreName(_player2Score)}";
     }
+
+    private static string GetScoreName(int tempScore) => tempScore switch
+    {
+        0 => "Love",
+        1 => "Fifteen",
+        2 => "Thirty",
+        _ => "Forty",
+    };
 }
